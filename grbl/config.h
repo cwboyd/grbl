930357a79d7bc6dc3f93b54af7acc08f0f833ad7
cwboyd <cwboyd@users.noreply.github.com>
2018-01-24 00:34:33 -0600

    Protoneer shield cannot support VARIABLE_SPINDLE, because it uses D11 for Z limit

--- grbl/config.h
+++ grbl/config.h
@@ -336,7 +336,9 @@
 // enable pin will output 5V for maximum RPM with 256 intermediate levels and 0V when disabled.
 // NOTE: IMPORTANT for Arduino Unos! When enabled, the Z-limit pin D11 and spindle enable pin D12 switch!
 // The hardware PWM output on pin D11 is required for variable spindle output voltages.
-#define VARIABLE_SPINDLE // Default enabled. Comment to disable.
+// VARIABLE_SPINDLE will not work for some pre-built Uno shields (namely the Protoneer board) which use
+// Uno D11 as the Z limit and D12 as the spindle enable.
+//#define VARIABLE_SPINDLE // Default enabled. Comment to disable.
 
 // Used by variable spindle output only. This forces the PWM output to a minimum duty cycle when enabled.
 // The PWM pin will still read 0V when the spindle is disabled. Most users will not need this option, but
